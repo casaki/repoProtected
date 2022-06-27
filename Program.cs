197e000a7dfa409690b2c4f142a073898df71fd8
Carlos Alberto Sanchez Quintana <casquintana@gmail.com>
2022-06-27 14:20:11 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-Console.Write("Buenos noches, Como te llamas?");
+Console.Write("Buenos dias, Como te llamas?");
 var name= Console.ReadLine();
 Console.Write($"Hola, {name}, te gusta programar?")
 var respuesta = Console.ReadLine();
